6ee8d5044330585e805daa29353c91528596779e
ruanfernandes2592 <ruanfernandes2592@gmail.com>
2025-04-23 00:05:35 -0300

    Teste adicionado

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,32 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste PostComment", () => {
+  test("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  test("Deve adicionar dois comentários", () => {
+    render(<PostComment />);
+
+    fireEvent.change(screen.getByTestId("textarea-comentario"), {
+      target: {
+        value: "Teste comentário adicionado",
+      },
     });
+
+    fireEvent.click(screen.getByTestId("btn-comentario"));
+
+    fireEvent.change(screen.getByTestId("textarea-comentario"), {
+      target: {
+        value: "Segundo teste comentário adicionado",
+      },
+    });
+
+    fireEvent.click(screen.getByTestId("btn-comentario"));
+
+    expect(screen.getAllByTestId("elemento-comentario")).toHaveLength(2);
+  });
 });
